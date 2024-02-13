c0712874de8e87ed385f7144a14fb8da93eca4bd
Damodhar Reddy <damodarreddy295@gmail.com>
2024-02-13 23:59:28 +0530

    category for user registration

--- youblog-apis/src/main/java/com/youblog/repositories/CategoryDetailsRepository.java
+++ youblog-apis/src/main/java/com/youblog/repositories/CategoryDetailsRepository.java
@@ -12,5 +12,10 @@ public interface CategoryDetailsRepository extends JpaRepository<CategoryDetails
 	@Query(value = "select * from category_details and post_details_flag = true", nativeQuery = true)
 	public List<CategoryDetailsEntity> getCategoryList();
 
+	@Query(value = "select * from category_details cd where cd.category_id not in \n"
+			+ "(select cd.category_id from category_details cd \n"
+			+ "inner join user_details ud on ud.category_id = cd.category_id and ud.active_flag = true and ud.gym_id = :gymId\n"
+			+ "where cd.user_details_flag = true) and\n"
+			+ "cd.user_details_flag = true",nativeQuery = true)
 	public List<CategoryDetailsEntity> userCategoryList(Long gymId);
 }
