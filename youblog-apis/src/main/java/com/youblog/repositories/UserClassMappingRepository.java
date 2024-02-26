bfa8f2df291c336c240dace9aadd6ec7a4785878
Damodhar Reddy <damodarreddy295@gmail.com>
2024-02-26 22:38:31 +0530

    class user mapping bug

--- youblog-apis/src/main/java/com/youblog/repositories/UserClassMappingRepository.java
+++ youblog-apis/src/main/java/com/youblog/repositories/UserClassMappingRepository.java
@@ -14,9 +14,9 @@ public interface UserClassMappingRepository extends JpaRepository<UserClassMappi
 	@Query(value = "select case when count(*)>0 then false else true end from class_details cd \r\n"
 			+ "inner join (select cd.week_day,cd.time_details_id from user_class_mapping ucm\r\n"
 			+ "inner join class_details cd on cd.class_details_id = ucm.class_details_id\r\n"
-			+ "and cd.active_flag = true where ucm.active_flag=true and ucm.user_id = :classDetailsId)clasd on \r\n"
+			+ "and cd.active_flag = true where ucm.active_flag=true and ucm.user_id = :userId)clasd on \r\n"
 			+ "clasd.week_day = cd.week_day and clasd.time_details_id = cd.time_details_id \r\n"
-			+ "where cd.active_flag = true and cd.class_details_id=:userId", nativeQuery = true)
+			+ "where cd.active_flag = true and cd.class_details_id=:classDetailsId", nativeQuery = true)
 	public Boolean checkForTime(Long userId, Long classDetailsId);
 
 	@Query(value = "select * from user_class_mapping where user_class_mapping_id = :userClassMappingId and active_flag = true", nativeQuery = true)
